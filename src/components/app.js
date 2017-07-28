f8e16b43583d7fa46d8d3e392278be5f5260f393
luchopintado <luchopintado@gmail.com>
2017-07-27 19:59:58 -0500

    Add BookList reducer

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import BookList from '../containers/book-list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <h1>React simple starter</h1>
+        <BookList />
+      </div>
     );
   }
 }
